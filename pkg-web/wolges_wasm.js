5a18aa555a27ff5cc51ffc1bc1b0489b9b63fd9b
Andy Kurnia <andykurn@gmail.com>
2023-04-16 23:42:26 +0800

    commit wolges-wasm f9709e8a9e9564ecb309850d50786d55e27047fa

--- pkg-web/wolges_wasm.js
+++ pkg-web/wolges_wasm.js
@@ -71,7 +71,7 @@ function makeMutClosure(arg0, arg1, dtor, f) {
     return real;
 }
 function __wbg_adapter_10(arg0, arg1, arg2) {
-    wasm._dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h7ca1a542a0f6edfd(arg0, arg1, addHeapObject(arg2));
+    wasm._dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hea198ec2aba6ac3c(arg0, arg1, addHeapObject(arg2));
 }
 
 /**
@@ -214,7 +214,7 @@ function handleError(f, args) {
     }
 }
 function __wbg_adapter_24(arg0, arg1, arg2, arg3) {
-    wasm.wasm_bindgen__convert__closures__invoke2_mut__h91be558819a8e05a(arg0, arg1, addHeapObject(arg2), addHeapObject(arg3));
+    wasm.wasm_bindgen__convert__closures__invoke2_mut__h7dc93f80f1d2f21a(arg0, arg1, addHeapObject(arg2), addHeapObject(arg3));
 }
 
 async function load(module, imports) {
