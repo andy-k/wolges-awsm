b92893d5b4fb2c037b227a444b08244dd3d7ceba
Andy Kurnia <andykurn@gmail.com>
2024-03-04 01:46:38 +0800

    commit wolges-wasm d9ece2ba735b92b54522606291014640b6defe2e

--- pkg-web/wolges_wasm.js
+++ pkg-web/wolges_wasm.js
@@ -377,7 +377,7 @@ function __wbg_get_imports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper337 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper339 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 67, __wbg_adapter_16);
         return addHeapObject(ret);
     };
