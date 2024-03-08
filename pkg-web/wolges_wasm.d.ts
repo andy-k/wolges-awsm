ade549d1fc3ba6f65c88b60cd07fd1667a7f53e7
Andy Kurnia <andykurn@gmail.com>
2024-03-08 23:43:34 +0800

    commit wolges-wasm 4f53f55a94cb5cfcf00fc9104352248c02a5268c

--- pkg-web/wolges_wasm.d.ts
+++ pkg-web/wolges_wasm.d.ts
@@ -34,13 +34,13 @@ export interface InitOutput {
   readonly analyze: (a: number, b: number) => number;
   readonly play_score: (a: number, b: number, c: number) => void;
   readonly __wbindgen_export_0: WebAssembly.Table;
-  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hf541efccf9213a9f: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h905d0f866daef3da: (a: number, b: number, c: number) => void;
   readonly __wbindgen_malloc: (a: number, b: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
   readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
   readonly __wbindgen_free: (a: number, b: number, c: number) => void;
   readonly __wbindgen_exn_store: (a: number) => void;
-  readonly wasm_bindgen__convert__closures__invoke2_mut__h20266e39809c0e2f: (a: number, b: number, c: number, d: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke2_mut__h4f697d61f8132556: (a: number, b: number, c: number, d: number) => void;
   readonly __wbindgen_start: () => void;
 }
 
