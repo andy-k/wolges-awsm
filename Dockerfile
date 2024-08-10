7add0decd7956a56d53e3adf2a1e53b635d4300a
Andy Kurnia <andykurn@gmail.com>
2024-08-10 10:48:37 +0800

    try bun

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
-from denoland/deno:1.45.5
+from oven/bun:1.1.22
 workdir /app
 copy src src
 copy pkg-web pkg-web
-run ["deno", "cache", "src/wolges.ts"]
-cmd ["run", "--allow-read", "--allow-net", "src/wolges.ts"]
+cmd ["run", "src/wolges.ts"]
--- src/wolges.ts
+++ src/wolges.ts
@@ -4,10 +4,11 @@ import init, {
   precache_klv,
   precache_kwg,
 } from "../pkg-web/wolges_wasm.js";
+import { readdir } from "node:fs/promises";
 
-await init(Deno.readFile("pkg-web/wolges_wasm_bg.wasm"));
+await init(Bun.file("pkg-web/wolges_wasm_bg.wasm").bytes());
 const cachePromises = [];
-for await (const dirEntry of Deno.readDir("data")) {
+for (const dirEntry of await readdir("data", { withFileTypes: true })) {
   if (dirEntry.isFile) {
     const m = dirEntry.name.match(/^(.*)\.(klv2|kwg|kad)$/);
     if (m) {
@@ -16,21 +17,21 @@ for await (const dirEntry of Deno.readDir("data")) {
           cachePromises.push((async () =>
             precache_klv(
               m[1],
-              await Deno.readFile(`data/${dirEntry.name}`),
+              await Bun.file(`data/${dirEntry.name}`).bytes(),
             ))());
           break;
         case "kwg":
           cachePromises.push((async () =>
             precache_kwg(
               m[1],
-              await Deno.readFile(`data/${dirEntry.name}`),
+              await Bun.file(`data/${dirEntry.name}`).bytes(),
             ))());
           break;
         case "kad":
           cachePromises.push((async () =>
             precache_kwg(
               `${m[1]}.WordSmog`,
-              await Deno.readFile(`data/${dirEntry.name}`),
+              await Bun.file(`data/${dirEntry.name}`).bytes(),
             ))());
           break;
       }
@@ -41,33 +42,36 @@ await Promise.all(cachePromises);
 
 const jsonResponseHeaders = { headers: { "Content-Type": "application/json" } };
 const pingResponseBody = JSON.stringify({ ok: true });
-Deno.serve({ port: 4500 }, async (req) => {
-  try {
-    switch (req.method) {
-      case "GET":
-        switch ((new URL(req.url)).pathname) {
-          case "/ping":
-            return new Response(pingResponseBody, jsonResponseHeaders);
-        }
-        break;
-      case "POST":
-        switch ((new URL(req.url)).pathname) {
-          case "/analyze":
-            return new Response(
-              await analyze(await req.text()),
-              jsonResponseHeaders,
-            );
-          case "/play-score":
-            return new Response(
-              await play_score(await req.text()),
-              jsonResponseHeaders,
-            );
-        }
-        break;
+Bun.serve({
+  port: 4500,
+  fetch: async (req) => {
+    try {
+      switch (req.method) {
+        case "GET":
+          switch ((new URL(req.url)).pathname) {
+            case "/ping":
+              return new Response(pingResponseBody, jsonResponseHeaders);
+          }
+          break;
+        case "POST":
+          switch ((new URL(req.url)).pathname) {
+            case "/analyze":
+              return new Response(
+                await analyze(await req.text()),
+                jsonResponseHeaders,
+              );
+            case "/play-score":
+              return new Response(
+                await play_score(await req.text()),
+                jsonResponseHeaders,
+              );
+          }
+          break;
+      }
+      return new Response("", { status: 404 });
+    } catch (e) {
+      console.error(new Date().toISOString(), "error:", e.stack, e, req);
+      return new Response(e.stack ?? e, { status: 500 });
     }
-    return new Response("", { status: 404 });
-  } catch (e) {
-    console.error(new Date().toISOString(), "error:", e.stack, e, req);
-    return new Response(e.stack ?? e, { status: 500 });
-  }
+  },
 });
